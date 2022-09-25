aa7b28acf9348bd31b8100d24ca9819e2bae7f03
Jhen Nunez <jxelektro@gmail.com>
2022-09-25 16:07:29 -0300

    8 **All. ++Line 4

--- src/__test__/all.test.js
+++ src/__test__/all.test.js
@@ -1,6 +1,7 @@
 import { render , screen } from "@testing-library/react";
 import '@testing-library/jest-dom/extend-expect';
 import All from "../Pages/All";
+import React from "react";
 
 // test the component All 
 test("renders All component", () => {
